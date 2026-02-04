61f9c8aa06ec44878bdb4cf79459bbff932ff436
Mike d'Sousa <mikedesousa92@gmail.com>
2026-02-04 18:30:38 -0300

    feat: coonfiguração inicial do redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import { TaskProvider } from './contexts/TaskContext'
-import { ThemeProvider } from './contexts/ThemeContext'
-import StudyPlannerPage from './pages/StudyPlannerPage'
+import { TaskProvider } from "./contexts/TaskContext";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import StudyPlannerPage from "./pages/StudyPlannerPage";
+import { store } from "./store";
+import { Provider } from "react-redux";
 
 function App() {
   return (
-    <ThemeProvider>
-      <TaskProvider>
-        <StudyPlannerPage />
-      </TaskProvider>
-    </ThemeProvider>
-  )
+    <Provider store={store}>
+      <ThemeProvider>
+        <TaskProvider>
+          <StudyPlannerPage />
+        </TaskProvider>
+      </ThemeProvider>
+    </Provider>
+  );
 }
 
-export default App
+export default App;
